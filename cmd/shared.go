671c469e4d40c455d061f9f07bda5f6725e8bd52
John Taylor Adm <jtaylor@uhs.uga.edu>
2021-02-02 14:43:43 -0500

    initial commit

---
+++ cmd/shared.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"encoding/csv"
+	mailingList "github.com/jftuga/quautomatrics/mailinglist"
+	"io"
+	"log"
+	"os"
+)
+
+// getCSVEntries - convert a CSV entry into an array of Contact
+func getCSVEntries() []mailingList.Contact {
+	file, err := os.Open(csvFile)
+	if err != nil {
+		log.Fatalf("Error #70055: unable to open ")
+	}
+	r := csv.NewReader(file)
+
+	var allCSVEntries []mailingList.Contact
+	line := 0
+	for {
+		line += 1
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error #70062: unable to process CSV entry: %s, line: %d\n%s\n", csvFile, line, err)
+		}
+		allCSVEntries = append(allCSVEntries, mailingList.Contact{"", record[2], record[0], record[1]})
+	}
+	return allCSVEntries
+}
+
