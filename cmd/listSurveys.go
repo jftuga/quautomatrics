5aedc7a4730395448f76e0b581c54038f9897969
John Taylor Adm <jtaylor@uhs.uga.edu>
2021-02-03 14:16:03 -0500

    fix copy/paste typo

--- cmd/listSurveys.go
+++ cmd/listSurveys.go
@@ -31,27 +31,22 @@ import (
 // listSurveysCmd represents the listSurveys command
 var listSurveysCmd = &cobra.Command{
 	Use:   "listSurveys",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: `Get a survey ID.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		//fmt.Println("listSurveys called")
 		listSurveys()
 	},
 }
 
+var surveyName string
+
 func init() {
 	rootCmd.AddCommand(listSurveysCmd)
-	listSurveysCmd.Flags().StringVarP(&libraryName, "name", "n", "", "preexisting survey name")
+	listSurveysCmd.Flags().StringVarP(&surveyName, "name", "n", "", "preexisting survey name")
 	listSurveysCmd.MarkFlagRequired("name")
 }
 
 func listSurveys() {
-	surveyName := libraryName
 	survey := survey.New(surveyName)
 	fmt.Println("surveyId:", survey.Id)
 }
