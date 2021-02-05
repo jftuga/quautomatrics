3e2d72111e8ef3e6d01b3680a644c85b555a2344
John Taylor Adm <jtaylor@uhs.uga.edu>
2021-02-05 08:30:21 -0500

    move upload function to its own separate command

--- cmd/createDistribution.go
+++ cmd/createDistribution.go
@@ -27,10 +27,8 @@ import (
 	"github.com/jftuga/quautomatrics/distribution"
 	"github.com/jftuga/quautomatrics/library"
 	mailingList "github.com/jftuga/quautomatrics/mailinglist"
-	"github.com/jftuga/quautomatrics/rest"
 	"github.com/jftuga/quautomatrics/survey"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"io/ioutil"
 	"log"
 	"os"
@@ -105,21 +103,7 @@ func init() {
 
 func createDistribution() {
 	envelope := validateParameters()
-	fmt.Println("envelope:", envelope)
-	fmt.Println()
-	dist := createDistributionEnvelope(envelope)
-	fmt.Println(dist)
-	fmt.Println()
-	uploadDistribution(dist)
-}
-
-func uploadDistribution(dist string) {
-	token := viper.GetString("X-API-TOKEN")
-	dc := viper.GetString("DATACENTER")
-	r := rest.New(token, dc)
-
-	status := r.Post("/distributions", []byte(dist))
-	fmt.Println("uploadDistribution status:", status)
+	createDistributionEnvelope(envelope)
 }
 
 func createDistributionEnvelope(envelope *DistributionEnvelope) string {
---
+++ cmd/uploadDistribution.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2021 John Taylor
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/jftuga/quautomatrics/rest"
+	"github.com/spf13/viper"
+	"io/ioutil"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// uploadDistributionCmd represents the uploadDistribution command
+var uploadDistributionCmd = &cobra.Command{
+	Use:   "uploadDistribution",
+	Short: "Upload a distribution file",
+	Long: `
+Once uploaded, the distribution will be active and emails will 
+be sent out starting at 'sendDate'`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// fmt.Println("uploadDistribution called")
+		uploadDistribution()
+	},
+}
+
+var distFile string
+
+func init() {
+	rootCmd.AddCommand(uploadDistributionCmd)
+	uploadDistributionCmd.Flags().StringVarP(&distFile, "distFile", "d", "", "JSON distribution file created with 'createDistribution'")
+	uploadDistributionCmd.MarkFlagRequired("distFile")
+}
+
+func uploadDistribution() {
+	dist, err := ioutil.ReadFile(distFile)
+	if err != nil {
+		log.Fatalf("Error #50092: unable to read distFile: '%s'\n%s\n", distFile, err)
+	}
+	token := viper.GetString("X-API-TOKEN")
+	dc := viper.GetString("DATACENTER")
+	r := rest.New(token, dc)
+
+	status := r.Post("/distributions", dist)
+	fmt.Println("uploadDistribution status:", status)
+}
