04fc794d88d0bbea0c5d60e7ff401783005ea1d2
John Taylor Adm <jtaylor@uhs.uga.edu>
2021-02-10 10:27:07 -0500

    version bump

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,7 +1,7 @@
 package cmd
 
 const pgmName string = "quautomatrics"
-const pgmVersion string = "1.1.1"
+const pgmVersion string = "1.2.0"
 const pgmURL string = "https://github.com/jftuga/quautomatrics"
 
 // used by createDistribution
